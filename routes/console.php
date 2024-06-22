a3420a50e59e81ba9e5a0b6f44535527c79f6d61
Gomonuka <julianagomonjuk@gmail.com>
2024-06-22 13:16:30 +0300

    Beginning of the development of welcome index file

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,31 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schema;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('MigrateInOrder', function () {
+    $migrations = [ 
+        '0001_01_01_000000_create_users_table.php',
+        '0001_01_01_000001_create_cache_table.php',
+        '0001_01_01_000002_create_jobs_table.php',
+        '2024_06_20_132850_create_categories_table.php',
+        '2024_06_20_132951_create_shelters_table.php',
+        '2024_06_20_132857_create_pets_table.php',
+        '2024_06_20_132904_create_blogposts_table.php',
+        '2024_06_20_132940_create_roles_table.php',
+        '2024_06_20_132945_create_tasks_table.php',
+        '2024_06_20_132935_create_comments_table.php'
+    ];
+    foreach($migrations as $migration) {
+        $basePath = 'database/migrations/';          
+        $migrationName = trim($migration);
+        $path = $basePath.$migrationName;
+        $this->call('migrate:refresh', [
+            '--path' => $path,            
+        ]);
+    }   
+})->purpose('Migrating tables in a specific order');
